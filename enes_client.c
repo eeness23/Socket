cdc8f25c760b429f228ef511e03fa7ce7c013846
Enes Demirbas <37370909+eeness23@users.noreply.github.com>
2018-03-14 16:27:29 +0300

    Update enes_client.c

--- enes_client.c
+++ enes_client.c
@@ -10,7 +10,7 @@
 #define mesaj_max 200
 
 int main(){
-	printf("\ If you want to turn off the client write "!" ");
+	printf("\n If you want to turn off the client write "!" ");
 	int soket,uzunluk;
 	soket=socket(AF_INET,SOCK_STREAM,0);
 	struct sockaddr_in serverAdress;
